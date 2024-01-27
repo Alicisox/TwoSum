dc2cec65c8e51f38aa6ab0eef2ce916931adf109
Natthapong Chantaklad <73196876+Alicisox@users.noreply.github.com>
2024-01-27 16:08:21 +1100

    Add an optimal solution and add a test case.

--- 1twoSum.py
+++ 1twoSum.py
@@ -6,11 +6,24 @@ class Solution:
                 if nums[j] == diff:
                     return [i,j]
 
+    def twoSum(self, nums, target):
+        hashIndex = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in hashIndex:
+                return [hashIndex[diff], i]
+            hashIndex[n] = i
 
+# Testing 
 nums = [2,7,11,15]
-target = 13
+target = 9
+
+nums = [3,3] 
+target = 6
 
 nums = [3,2,4]
 target = 6
 
 print(Solution().twoSum_bruteforce(nums, target))
+
+print(Solution().twoSum(nums, target))
